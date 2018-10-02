89c5b85d1c9c1dbf4b7e0d706d5ff09fc69e81f6
Wxuanzhi <user@es302n26>
2018-10-02 10:47:14 +0800

    variables

--- myproject/src/com/wxuan/Hello.java
+++ myproject/src/com/wxuan/Hello.java
@@ -5,6 +5,18 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World");
+        int age = 18;
+        float weight = 47f;
+        float height = 153.4f;
+        String name = "Wxuanzhi";
+        int schoolYear = 107;
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(name);
+        System.out.println(schoolYear);
+
+        		
 	}
 
 }
